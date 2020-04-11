e2c66383b59c409e051c129dfafb6e5e1dc3e536
Jacob <jgsimmerman@gmail.com>
2020-04-11 19:25:29 -0400

    rebuild

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,25 +7,25 @@ sharp.simd(false);
 
 const glob = require('glob');
 
-exports.onPostBuild = () => {
-  // Configure where the functions are kept and where we want to move them.
-  const srcLocation = `${__dirname}/src/functions`;
-  const outputLocation = `${__dirname}/public/functions`;
+// exports.onPostBuild = () => {
+//   // Configure where the functions are kept and where we want to move them.
+//   const srcLocation = `${__dirname}/src/functions`;
+//   const outputLocation = `${__dirname}/public/functions`;
 
-  // Get all the functions.
-  const modules = glob.sync('*.js', { cwd: srcLocation });
-  modules.forEach(src => {
-    const moduleSrc = path.join(srcLocation, src);
-    const moduleOut = path.join(outputLocation, path.basename(src, path.extname(src)) + '.js');
+//   // Get all the functions.
+//   const modules = glob.sync('*.js', { cwd: srcLocation });
+//   modules.forEach(src => {
+//     const moduleSrc = path.join(srcLocation, src);
+//     const moduleOut = path.join(outputLocation, path.basename(src, path.extname(src)) + '.js');
 
-    // Copy file to new location.
-    fs.copyFile(moduleSrc, moduleOut, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-  });
-};
+//     // Copy file to new location.
+//     fs.copyFile(moduleSrc, moduleOut, (err) => {
+//       if (err) {
+//         throw err;
+//       }
+//     });
+//   });
+// };
 
 
 
