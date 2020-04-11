bdd96d46d595f04f2ec5e729e81657abc3cc2bd6
Jacob <jgsimmerman@gmail.com>
2020-04-11 19:06:43 -0400

    rebuild

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,15 +5,27 @@ const fs = require('fs');
 sharp.cache(false);
 sharp.simd(false);
 
-// const { zipFunctions } = require('@netlify/zip-it-and-ship-it');
-// exports.onPostBuild = () => {
-//   const srcLocation = path.join(__dirname, `./cart/functions`);
-//   const outputLocation = path.join(__dirname, `./public/functions`);
-//   if (!fs.existsSync(outputLocation)) {
-//     fs.mkdirSync(outputLocation);
-//   }
-//   return zipFunctions(srcLocation, outputLocation);
-// };
+const glob = require('glob');
+
+exports.onPostBuild = () => {
+  // Configure where the functions are kept and where we want to move them.
+  const srcLocation = `${__dirname}/src/functions`;
+  const outputLocation = `${__dirname}/public/functions`;
+
+  // Get all the functions.
+  const modules = glob.sync('*.js', { cwd: srcLocation });
+  modules.forEach(src => {
+    const moduleSrc = path.join(srcLocation, src);
+    const moduleOut = path.join(outputLocation, path.basename(src, path.extname(src)) + '.js');
+
+    // Copy file to new location.
+    fs.copyFile(moduleSrc, moduleOut, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  });
+};
 
 
 
